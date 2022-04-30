d94668b71477cbf0b2f3ef0e9b60bc2d42bab20f
renankuba <kuba.renan@gmail.com>
2022-04-30 22:00:08 +0100

    fix dpad size and add its center element

--- src/pokedex/button/dpad/Dpad.tsx
+++ src/pokedex/button/dpad/Dpad.tsx
@@ -3,9 +3,13 @@ import './Dpad.css'
 const Dpad = () => <div className="d-pad">
     <button className={`poke-button arrow up`}/>
     <button className={`poke-button arrow left`}/>
-    <div className="center"></div>
     <button className={`poke-button arrow right`}/>
     <button className={`poke-button arrow down`}/>
+    <div className="center-wrapper">
+        <button 
+            className='poke-button center'
+            disabled={true} />
+    </div>
 </div>
 
 export default Dpad;
